fd96a8488098dd7e5e5dccc0b8ab674ad9c9b4be
Yellow7Y <102256640+Yellow7Y@users.noreply.github.com>
2022-06-13 16:14:44 -0300

    Update CorretoraSeguros.java

--- CorretoraSeguros.java
+++ CorretoraSeguros.java
@@ -6,7 +6,17 @@ Veja que o método fazerPropostaSeguro() recebe como parâmetro um objeto do tip
 mas no futuro poderia receber também uma Aeronave ou Barco, desde que essas classes implementem a interface Seguravel.
  */
 public class CorretoraSeguros implements Seguravel {
-    void fazerPropostaSeguro() {
+    private int IdadeCondutor;
+    private Object CorretoraSeguros;
 
+    Double fazerPropostaSeguro() {
+        if (IdadeCondutor == 18 || IdadeCondutor == 19 || IdadeCondutor == 20 ||IdadeCondutor == 21 ||IdadeCondutor == 22 ||IdadeCondutor == 23 ||IdadeCondutor == 24) {
+            CorretoraSeguros + 5,26%;
+        }
+        //   if IdadeCondutor 18 a 24 anos
+        //   else IdadeCondutor 25 a 49 anos
+        //    else IdadeCondutor 50 a 80 anos
+        //    else IdadeCondutor 81 a 120 anos não e possivel realizar o seguro.
+        return null;
     }
 }
